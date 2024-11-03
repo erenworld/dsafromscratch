db72d88fa3c0ed0b78d57cbe78c428150beadc41
Eren Turkoglu <erenxturkoglu@gmail.com>
2024-11-03 21:07:51 +0100

    ADD

--- sorting/insertion/insertion.c
+++ sorting/insertion/insertion.c
@@ -7,3 +7,17 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+
+void insertion(int arr[], int len) 
+{
+    for (int i = 1; i < len - 1; i++) {
+        int value = arr[i];
+        int hole = i;
+
+        while (hole > 0 && arr[hole - 1] > value) {
+            arr[hole] = arr[hole - 1];
+            hole = hole - 1;
+        }
+        arr[hole] = value;
+    }
+}
